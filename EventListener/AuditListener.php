501f5f87fa6e6991f9f064f18caf896a4cc6b483
Jolly Blume <jollyblume2@gmail.com>
2019-01-30 13:59:21 -0700

    skeleton audit listener

---
+++ EventListener/AuditListener.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace JBJ\Workflow\EventListener;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use JBJ\Workflow\Event\BackendEvent as Event;
+
+class AuditListener implements EventSubscriberInterface
+{
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    public function onGet(Event $event)
+    {
+    }
+
+    public function onSet(Event $event)
+    {
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            'workflow.backend.get' => ['onGet'],
+            'workflow.backend.set' => ['onSet'],
+        ];
+    }
+}
