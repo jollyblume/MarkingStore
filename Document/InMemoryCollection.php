fe04d0609f53f41fcee957f311088b743019a819
Jolly Blume <jollyblume2@gmail.com>
2019-01-30 22:39:09 -0700

    default persist strategy

--- Document/InMemoryCollection.php
+++ Document/InMemoryCollection.php
@@ -2,7 +2,16 @@
 
 namespace JBJ\Workflow\Document;
 
-class InMemoryCollection
+use JBJ\Common\Collection\ArrayCollectionInterface;
+
+class InMemoryCollection implements ArrayCollectionInterface
 {
-    
+    use JBJ\Common\Collection\CollectionTrait;
+
+    public function __construct(array $elements = [])
+    {
+        if (!empty($elements)) {
+            $this->setChildren($elements);
+        }
+    }
 }
--- PersistStrategy/InMemoryStrategy.php
+++ PersistStrategy/InMemoryStrategy.php
@@ -4,35 +4,38 @@ namespace JBJ\Workflow\PersistStrategy;
 
 use Psr\Log\LoggerInterface;
 use JBJ\Workflow\PersistStrategyInterface;
+use JBJ\Workflow\Document\InMemoryCollection;
 
 class InMemoryStrategy implements PersistStrategyInterface
 {
     private $logger;
+    private $collection;
 
-    public function __construct(LoggerInterface $logger)
+    public function __construct(LoggerInterface $logger, ArrayCollectionInterface $collection)
     {
         $this->logger = $logger;
-
+        $this->collection = $collection;
         $this->logger->info('InMemoryStrategy created');
     }
 
     public function storeCreated(string $markingStoreId, string $subjectId, array $places)
-    {
-
-    }
+    {}
 
     public function getPlaces(string $markingStoreId, string $subjectId)
     {
-
+        $index = sprintf('%s/%s', $markingStoreId, $subjectId);
+        $collection = $this->collection;
+        $places = $collection[$index] ?: [];
+        return $places;
     }
 
     public function setPlaces(string $markingStoreId, string $subjectId, array $places)
     {
-
+        $index = sprintf('%s/%s', $markingStoreId, $subjectId);
+        $collection = $this->collection;
+        $collection[$index] = $places;
     }
 
     public function flush()
-    {
-
-    }
+    {}
 }
