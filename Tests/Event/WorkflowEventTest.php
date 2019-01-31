ea467b11630ee6691e967f6f29b87cd9dce827c2
Jolly Blume <jollyblume2@gmail.com>
2019-01-31 00:12:46 -0700

    more tests

---
+++ Tests/Event/WorkflowEventTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace JBJ\Workflow\Tests\Event;
+
+use JBJ\Workflow\Event\WorkflowEvent;
+use PHPUnit\Framework\TestCase;
+
+class WorkflowEventTest extends TestCase
+{
+    public function testGetMarkingStoreId() {
+        $event = new WorkflowEvent('test.marking-store-id');
+        $markingStoreId = $event->getMarkingStoreId();
+        $this->assertEquals('test.marking-store-id', $markingStoreId);
+    }
+
+    public function testGetSubjectId() {
+        $event = new WorkflowEvent('test.marking-store-id', 'test.subject-id');
+        $subjectId = $event->getSubjectId();
+        $this->assertEquals('test.subject-id', $subjectId);
+    }
+
+    public function testGetPlaces() {
+        $expectedPlaces = [
+            'this.place',
+            'that.place',
+            'high.place',
+            'low.place',
+        ];
+        $event = new WorkflowEvent('test.marking-store-id', 'test.subject-id', $expectedPlaces);
+        $places = $event->getPlaces();
+        $this->assertEquals($expectedPlaces, $places);
+    }
+}
