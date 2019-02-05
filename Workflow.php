1cc2a68d7841de735b80931d9265595c1b5547f0
Jolly Blume <jollyblume2@gmail.com>
2019-02-05 02:28:36 -0700

    added parent and accessors

--- Workflow.php
+++ Workflow.php
@@ -2,9 +2,11 @@
 
 namespace JBJ\Workflow;
 
+use Symfony\Component\Workflow\Definition;
 use Symfony\Component\Workflow\Workflow as BaseWorkflow;
 use Symfony\Component\PropertyAccess\PropertyAccessorInterface;
 use Symfony\Component\PropertyAccess\PropertyAccess;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use JBJ\Workflow\MarkingStore\MarkingStoreShim;
 use JBJ\Workflow\Traits\CreateIdTrait;
 
@@ -12,17 +14,28 @@ class Workflow extends BaseWorkflow
 {
     use CreateIdTrait;
 
-    public function __construct(Definition $definition EventDispatcherInterface $dispatcher = null, PropertyAccessorInterface $propertyAccessor = null)
+    private $parent;
+
+    public function __construct(Definition $definition, EventDispatcherInterface $dispatcher = null, PropertyAccessorInterface $propertyAccessor = null)
     {
         $propertyAccessor = $propertyAccessor ?: PropertyAccess::createPropertyAccessor();
         $metadata = $definition->getMetadataStore()->getWorkflowMetadata()
         if (array_key_exists('name', $metadata)) {
             $name = $metadata['name'];
         }
-        //todo exception if no name in metadata. definition, workflow and marking
-        //     store name must remain synced.
         $name = $this->createId($name); // ensure $name is a uuid
         $markingStore = new MarkingStoreShim($dispatcher, $propertyAccessor, 'subjectId', $name);
         parent::__construct($definition, $markingStore, $dispatcher, $name);
+    }
+
+    public function getParent()
+    {
+        return $this->parent;
+    }
+
+    public function setParent($parent)
+    {
+        //todo input testing
+        $this->parent = $parent;
     }
 }
