3a67a6a28f6f0c7b5c6ba248901c78ccf9227ea9
Jolly Blume <jollyblume2@gmail.com>
2019-02-05 02:12:40 -0700

    skeleton collection

---
+++ WorkflowCollection.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace JBJ\Workflow;
+
+use JBJ\Common\Collection\ArrayCollectionInterface;
+use JBJ\Common\Collection\GraphCollectionTrait;
+use JBJ\Workflow\Traits\CreateIdTrait;
+use Symfony\Component\PropertyAccess\PropertyAccessorInterface;
+use Ramsey\Uuid\Uuid;
+
+class WorkflowCollection implements ArrayCollectionInterface
+{
+    use GraphCollectionTrait, GraphCollectionTrait { getName as public; getParent as public; setParent as public; }
+
+    public function __construct(array $elements = [], PropertyAccessorInterface $propertyAccessor = null, string $name = '')
+    {
+        $name = $this->createId($name);
+        $rules = [
+            'name' => [
+                'name',
+                'isDisabled' => false,
+                'isValid' => true,
+            ],
+            'parent' => [
+                'parent',
+                'isDisabled' => false,
+                'isValid' => true,
+            ],
+        ];
+        $this->initializeTrait($name, $elements, $rules, $propertyAccessor);
+    }
+}
