d61fd0b86af8d7e531d734a530205710601f5ba1
Jolly Blume <jollyblume2@gmail.com>
2019-02-08 10:22:13 -0700

    externalize index transforms

--- StorageStrategy/InMemoryStrategy.php
+++ StorageStrategy/InMemoryStrategy.php
@@ -2,6 +2,7 @@
 
 namespace JBJ\Workflow\MarkingStore\StorageStrategy;
 
+use Closure;
 use Psr\Log\LoggerInterface;
 use JBJ\Workflow\MarkingStore\StorageStrategyInterface;
 use JBJ\Workflow\MarkingStore\Document\InMemoryCollection;
@@ -16,14 +17,38 @@ class InMemoryStrategy implements StorageStrategyInterface
         $this->logger = $logger;
         $this->collection = [];
         $this->logger->info('InMemoryStrategy created');
+        $this->setDefaultIndexTransformer();
     }
 
     public function storeCreated(string $markingStoreId, string $subjectId, array $places)
-    {}
+    {
+    }
+
+    protected function setDefaultIndexTransformer()
+    {
+        $transformer = function(string $markingStoreId, string $subjectId) {
+            $index = sprintf('%s/%s', $markingStoreId, $subjectId);
+            return $index;
+        };
+        $this->setIndexTransformer($transformer);
+    }
+
+    private $transformer;
+    public function setIndexTransformer(Closure $transformer)
+    {
+        $this->transformer = $transformer;
+    }
+
+    protected function createIndex(string $markingStoreId, string $subjectId)
+    {
+        $transformer = $this->transformer;
+        $index = strval($transformer($markingStoreId, $subjectId));
+        return $index;
+    }
 
     public function getPlaces(string $markingStoreId, string $subjectId)
     {
-        $index = sprintf('%s/%s', $markingStoreId, $subjectId);
+        $index = $this->createIndex($markingStoreId, $subjectId);
         if (array_key_exists($index, $this->collection)) {
             return $this->collection[$index] ?: [];
         }
@@ -32,7 +57,7 @@ class InMemoryStrategy implements StorageStrategyInterface
 
     public function setPlaces(string $markingStoreId, string $subjectId, array $places)
     {
-        $index = sprintf('%s/%s', $markingStoreId, $subjectId);
+        $index = $this->createIndex($markingStoreId, $subjectId);
         if (empty($places)) {
             unset($this->collection[$index]);
         }
@@ -42,5 +67,6 @@ class InMemoryStrategy implements StorageStrategyInterface
     }
 
     public function cleanup()
-    {}
+    {
+    }
 }
--- StorageStrategyInterface.php
+++ StorageStrategyInterface.php
@@ -2,10 +2,13 @@
 
 namespace JBJ\Workflow\MarkingStore;
 
+use Closure;
+
 interface StorageStrategyInterface
 {
     public function storeCreated(string $markingStoreId, string $subjectId, array $places);
     public function getPlaces(string $markingStoreId, string $subjectId);
     public function setPlaces(string $markingStoreId, string $subjectId, array $places);
     public function cleanup();
+    public function setIndexTransformer(Closure $transformer);
 }
